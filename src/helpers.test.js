9620aa1c7252c7c0a77c24cfe9096dbb90689953
EstebanP <epinczinger@gmail.com>
2020-11-30 11:48:16 -0300

    reset local storage before each test

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -4,7 +4,12 @@
 
 import { updateLocalStorage } from './helpers'
 
+beforeEach(() => { 
+  localStorage.setItem('key', '');
+});
+
 test("update local storage array", () => {
+  expect(localStorage.getItem('key')).toBe('');
 
   const infoArray = [["key", "value"]];
   updateLocalStorage(infoArray);
